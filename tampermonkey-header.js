ed4ceae35c3bf7ddd9197577462d095d0c7c1ba7
Kobe Peng <pengchuanhui2010@gmail.com>
2022-11-19 00:46:40 +0800

    change name update url

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -1,9 +1,9 @@
 module.exports = {
   headers: {
-    name: "FUT Auto Buyer",
+    name: "FUT Auto Buyer Kobe",
     namespace: "http://tampermonkey.net/",
     version: "1.5.4",
-    description: "FUT Auto Buyer",
+    description: "FUT Auto Buyer Kobe",
     author: "CK Algos",
     match: [
       "https://www.ea.com/*/fifa/ultimate-team/web-app/*",
@@ -24,12 +24,12 @@ module.exports = {
       "https://code.jquery.com/jquery-3.6.1.min.js",
       "https://raw.githubusercontent.com/ckalgos/FUT-Auto-Buyer/main/external/discord.11.4.2.min.js",
       "https://greasyfork.org/scripts/47911-font-awesome-all-js/code/Font-awesome%20AllJs.js?version=275337",
-      "https://github.com/ckalgos/fut-trade-enhancer/releases/latest/download/fut-trade-enhancer.user.js",
+      "https://github.com/skyfyl/fut-trade-enhancer/releases/latest/download/fut-trade-enhancer.user.js",
     ],
     updateURL:
-      "https://github.com/ckalgos/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
+      "https://github.com/skyfyl/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
     downloadURL:
-      "https://github.com/ckalgos/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
+      "https://github.com/skyfyl/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
     noFrame: true,
   },
 };
